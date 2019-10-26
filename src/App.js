a2647e695579118b3811a56a3806c8ecc6615f06
NAQVI <nomanalam7214@gmail.com>
2019-10-26 22:30:08 +0200

    added new route and added Switch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import {BrowserRouter as Router,Route} from "react-router-dom"
+import {BrowserRouter as Router,Route,Switch} from "react-router-dom"
 import MainPage from './components/MainPage';
 import LoginSignup from './components/login-signup';
+import BookItem from "./components/BookItem"
 
 class App extends React.Component {
  
   render(){
     return (
       <Router>
+        <Switch> 
    <Route exact path="/" component={LoginSignup} />
     <Route path="/mainpage" component={MainPage}/>
+    <Route path="/:booktitle" component={BookItem}/>
+    </Switch>
     </Router>
   );
   }
