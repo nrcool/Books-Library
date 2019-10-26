e24860035831e73074632f5fb77d3bf0a305a84a
NAQVI <nomanalam7214@gmail.com>
2019-10-26 22:23:29 +0200

    added signout function

--- src/components/ProfileIcon.jsx
+++ src/components/ProfileIcon.jsx
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
 import {Dropdown,DropdownButton} from "react-bootstrap"
+import {withRouter} from "react-router-dom"
 
-export default class ProfileIcon extends Component {
+class ProfileIcon extends Component {
+    handleSignout=()=>{
+        localStorage.removeItem("userToken")
+        this.props.history.push("/")
+    }
     render() {
         return (
             <DropdownButton id="dropdown-basic-button" title="Profile">
             <Dropdown.Item href="#/action-1">Account</Dropdown.Item>
             <Dropdown.Item href="#/action-2">Books Borrowed</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">SignOut</Dropdown.Item>
+            <Dropdown.Item onClick={this.handleSignout}>SignOut</Dropdown.Item>
           </DropdownButton>
         )
     }
 }
+
+export default withRouter(ProfileIcon)
