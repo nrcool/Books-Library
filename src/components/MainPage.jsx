cb55a56de28a46dab9edb7ebe73b967059ab6ec6
NAQVI <nomanalam7214@gmail.com>
2019-10-26 22:25:24 +0200

    removed pagination component from this component

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -2,19 +2,20 @@
   import React, { Component } from 'react'
   import image from "../img/2.jpg"
 import Navigationbar from './Navbar'
-import PaginationBasic from './Pagination'
 import Books from './Books'
-
-
-  export default class MainPage extends Component {
+import {connect} from "react-redux"
+import {Redirect} from "react-router-dom"
+  class MainPage extends Component {
       render() {
           return (
             <div className="mainpage"style={{backgroundImage:`url(${image})`,backgroundSize:"cover"}}> 
-            <Navigationbar/>
-            <Books/>
-            <PaginationBasic/>
+           {this.props.loginUser?<> <Navigationbar/>
+            <Books/> </>: <Redirect to="/"/>}
           </div>
           )
       }
   }
-  
+  const mapStateToProps=(state)=>{
+    return state
+  }
+  export default connect(mapStateToProps)(MainPage)
