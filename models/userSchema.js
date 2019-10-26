54540c41018fd7f8ce9fa0fff6dc71b273fa6472
NAQVI <nomanalam7214@gmail.com>
2019-10-26 22:31:19 +0200

    added username in user Schema

--- models/userSchema.js
+++ models/userSchema.js
@@ -2,6 +2,11 @@ const mongoose=require("mongoose");
 const Schema=mongoose.Schema
 
 const User=new Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
     email:{
         type:String,
         required:true,
