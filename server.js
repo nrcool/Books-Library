4cdb2618542b0a5a6d1398073d2543f20d0edba5
NAQVI <nomanalam7214@gmail.com>
2019-10-26 22:18:24 +0200

    added BookRoute

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const {errorController}= require("./errorHandler/errorHandler")
 const cors=require("cors")
 const port = process.env.PORT || 4000;
 const userRoute=require("./routes/userRoute")
+const bookRouter=require("./routes/booksRoute")
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"))
@@ -13,7 +14,7 @@ app.use(cors({origin: 'http://localhost:3000'}))
 
 
 app.use("/",cors(),userRoute,errorController)
-
+app.use("/books",bookRouter,errorController)
 
 app.listen(port,()=>{
     console.log("server is running "+port)
