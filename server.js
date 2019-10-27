a523556d34a4a7110fa588bf0e49294b0c9abd96
NAQVI <nomanalam7214@gmail.com>
2019-10-27 13:44:12 +0100

    changed urlencoded proptery to true.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 4000;
 const userRoute=require("./routes/userRoute")
 const bookRouter=require("./routes/booksRoute")
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"))
 app.use(cors({origin: 'http://localhost:3000'}))
 
