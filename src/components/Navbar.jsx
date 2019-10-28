9f614eb07e984fbb43531e2c0625863c09679531
NAQVI <nomanalam7214@gmail.com>
2019-10-28 12:09:25 +0100

    fetching data from server by authors name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,61 @@
 import React from "react"
 import {Nav,Form,FormControl,Navbar,Button} from "react-bootstrap"
 import ProfileIcon from "./ProfileIcon"
+import { connect } from "react-redux"
 
 
-export default function Navigationbar() {
+ class Navigationbar extends React.Component{
+   state={
+     search:""
+   }
+  searchbook=(e)=>{
+    e.preventDefault()
+    fetch("http://localhost:4000/books/search",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({search:this.state.search})})
+    .then(res=>res.json())
+    .then(res1=>{
+      this.props.SearchedBook([res1.book])
+      console.log(res1)})
+  }
+
+  SortbyAuthor=()=>{
+    fetch("http://localhost:4000/books/searchbyauthor",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({startIndex:this.props.currentPage,endIndex:this.props.nextPage})})
+    .then(res=>res.json())
+    .then(res1=>{
+      this.props.SearchedBook(res1.books)
+      console.log(res1)})
+  }
+  SortbyTitle=()=>{
+    fetch("http://localhost:4000/books/searchbytitle",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({startIndex:this.props.currentPage,endIndex:this.props.nextPage})})
+    .then(res=>res.json())
+    .then(res1=>{
+      this.props.SearchedBook(res1.books)
+      console.log(res1)})
+  }
+  render(){
     return (
         <Navbar bg="dark" variant="dark" style={{width:"85vw"}}>
               <Navbar.Brand href="#home">Books Shelf</Navbar.Brand>
               <Nav className="mr-auto">
-                <Nav.Link href="#home">Books</Nav.Link>
-                <Nav.Link href="#features">By Title</Nav.Link>
-                <Nav.Link href="#pricing">By Author Name</Nav.Link>
+                <Nav.Link href="/mainpage">Books</Nav.Link>
+                <Nav.Link href="#/mainpage/bytitle" onClick={this.SortbyTitle}>By Title</Nav.Link>
+                <Nav.Link href="#/mainpage/byauthor" onClick={this.SortbyAuthor}>By Author Name</Nav.Link>
               </Nav>
-              <Form inline onSubmit={()=>console.log("Search")}>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Form inline onSubmit={this.searchbook}>
+                <FormControl type="text" onChange={(e)=>this.setState({search:e.target.value})} placeholder="Search" className="mr-sm-2" />
                 <Button type="submit" variant="outline-info">Search</Button>
                 <ProfileIcon/>
               </Form>
             </Navbar>
     )
 }
+  }
+
+  const mapDispatchToProps=(dispatch)=>{
+      return{
+        SearchedBook:(book)=>dispatch({type:"searchbook",payload:book})
+      }
+  }
+  const mapStateToProps=(state)=>{
+    return state
+  }
+  export default connect(mapStateToProps,mapDispatchToProps)(Navigationbar)
