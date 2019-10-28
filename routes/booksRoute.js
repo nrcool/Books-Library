960976a94f6e2bc1df4a56af6644d1f0a11c4e8e
NAQVI <nomanalam7214@gmail.com>
2019-10-28 12:08:08 +0100

    added one more route searchbyauthor

--- routes/booksRoute.js
+++ routes/booksRoute.js
@@ -1,10 +1,11 @@
 const express=require("express");
 const booksRouter=express.Router();
-const {getBooks,Borrowbook,search,sortByTitle}=require("../controllers/books")
+const {getBooks,Borrowbook,search,sortByTitle,sortByAuthor}=require("../controllers/books")
 
 
 booksRouter.get("/",getBooks)
 booksRouter.post("/bookborrow",Borrowbook)
 booksRouter.post("/search",search)
 booksRouter.post("/searchbytitle",sortByTitle)
+booksRouter.post("/searchbyauthor",sortByAuthor)
 module.exports = booksRouter
