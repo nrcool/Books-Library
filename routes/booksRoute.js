2dc652839f902e47ff00fd6ccd121e862227c765
NAQVI <nomanalam7214@gmail.com>
2019-10-28 11:59:24 +0100

    added two more routes

--- routes/booksRoute.js
+++ routes/booksRoute.js
@@ -1,9 +1,10 @@
 const express=require("express");
 const booksRouter=express.Router();
-const {getBooks,Borrowbook}=require("../controllers/books")
+const {getBooks,Borrowbook,search,sortByTitle}=require("../controllers/books")
 
 
 booksRouter.get("/",getBooks)
 booksRouter.post("/bookborrow",Borrowbook)
-
+booksRouter.post("/search",search)
+booksRouter.post("/searchbytitle",sortByTitle)
 module.exports = booksRouter
