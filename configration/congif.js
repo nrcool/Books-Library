54911f38d461b19a6fa2206908252b94b26de766
NAQVI <nomanalam7214@gmail.com>
2019-10-30 19:01:17 +0100

    added secret key

--- configration/congif.js
+++ configration/congif.js
@@ -2,11 +2,13 @@
 module.exports = () => {
     if (process.env.NODE_ENV === "production") {
         return { db: process.env.MONGO_KEY,
-                server:"" }
+                server:"",
+                secretKey:process.env.SECRET_KEY }
     } else {
         return ({
             db: "mongodb://127.0.0.1:27017/BookShelf",
-            server:"http://localhost:4000"
+            server:"http://localhost:4000",
+            secretKey:"TopSecret"
         })
     }
 }
