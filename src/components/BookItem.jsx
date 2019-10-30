db7d9f5eb618a617e02be328abcf5e89668d41dd
NAQVI <nomanalam7214@gmail.com>
2019-10-30 19:06:31 +0100

    added styling

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -7,11 +7,12 @@ export default function BookItem({location}) {
     const {book}=location.state
     return (
         <div className="bookItem" style={{minHeight:"100vh",width:"100vw",color:"white",backgroundImage:`url(${image})`,backgroundSize:"cover"}}>
+             <img className="imagebook" src={book.thumbnailUrl} width="150" height="200" alt="book"/>
            <h1 className="bg-dark">{book.title}</h1>
            <div > <p>{book.longDescription}</p></div>
            
-           <h2>{book.pageCount}</h2>
-           <img src={book.thumbnailUrl} alt="book"/>
+           <h4 className="bg-dark">Number of Pages :{book.pageCount}</h4>
+         
         </div>
     )
 }
