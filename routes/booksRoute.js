a43025a87f1ba2e106b8917d64d91afd74776384
NAQVI <nomanalam7214@gmail.com>
2019-11-03 20:25:35 +0100

    route link to return book

--- routes/booksRoute.js
+++ routes/booksRoute.js
@@ -1,6 +1,6 @@
 const express=require("express");
 const booksRouter=express.Router();
-const {getBooks,Borrowbook,search,sortByTitle,sortByAuthor}=require("../controllers/books")
+const {getBooks,Borrowbook,search,sortByTitle,sortByAuthor,bookReturn}=require("../controllers/books")
 
 
 booksRouter.get("/",getBooks)
@@ -8,4 +8,5 @@ booksRouter.post("/bookborrow",Borrowbook)
 booksRouter.post("/search",search)
 booksRouter.post("/searchbytitle",sortByTitle)
 booksRouter.post("/searchbyauthor",sortByAuthor)
+booksRouter.post("/returnbook",bookReturn)
 module.exports = booksRouter
